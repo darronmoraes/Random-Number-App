a730d154d3fddd20ae7849a1bd79301d08482afc
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-02-17 17:35:21 +0530

    sharing the data using implicit intents

--- app/src/main/java/com/example/luckyapp/LuckyNumberActivity.java
+++ app/src/main/java/com/example/luckyapp/LuckyNumberActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,12 +29,36 @@ public class LuckyNumberActivity extends AppCompatActivity {
         String name = i.getStringExtra("name");
 
         // Random number
-        tv_luckyNumber.setText("" + generateRandomNumber());
+        int storeRandomNumber = generateRandomNumber();
+        tv_luckyNumber.setText("" + storeRandomNumber);
+
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // call to shareData method
+                shareData(name, storeRandomNumber);
+            }
+        });
     }
 
     public int generateRandomNumber() {
         Random random = new Random();   // Object of Random class
         int upperLimit = 1000;
         return random.nextInt(upperLimit);
+    }
+
+    // Share Data functionality
+    public void shareData(String name, int randomNumber) {
+        // Implicit Intents
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");  // Text type setting
+
+        // Convert the int to string
+        String number = String.valueOf(randomNumber);
+
+        i.putExtra(Intent.EXTRA_SUBJECT, name + " got lucky today!");
+        i.putExtra(Intent.EXTRA_TEXT, name + " Lucky number is " + number);
+
+        startActivity(Intent.createChooser(i, "choose a platform"));
     }
 }
